8d5c258f482d6ef4e6b2f5b65ed50fa9b7e1b24b
Rəşid Seyidli <reshid.seyidli@gmail.com>
2023-04-23 21:50:47 +0400

    optimization

--- src/main/java/AynaPdf.java
+++ src/main/java/AynaPdf.java
@@ -7,104 +7,233 @@ import com.itextpdf.kernel.pdf.PdfPage;
 import com.itextpdf.kernel.pdf.PdfWriter;
 import com.itextpdf.kernel.pdf.canvas.PdfCanvas;
 import com.itextpdf.layout.Document;
+import com.itextpdf.kernel.colors.ColorConstants;
 
 import java.io.IOException;
 
 public class AynaPdf {
+
+    private static PdfCanvas canvas;
+    private static final float CHAR_LENGTH = 3.7f;
+    private static float center;
+    private static float centerilizedStartPoint;
 
     public void createPdf(Entity entity) throws IOException {
         String pdfPath = "ayna.pdf";
         PdfWriter pdfWriter = new PdfWriter(pdfPath);
         PdfDocument pdfDocument = new PdfDocument(pdfWriter);
 
-        PageSize pageSize = PageSize.A5;
+        PageSize pageSize = PageSize.A5; //A5 size in pixels at 72 DPI: 420 x 595 pixels.
         PdfPage page = pdfDocument.addNewPage(pageSize);
 
         // Add the dot to the page
-        PdfCanvas canvas = new PdfCanvas(page);
+        canvas = new PdfCanvas(page);
         String fontPath = "fonts/roboto-regular.ttf";
         PdfFont font = PdfFontFactory.createFont(fontPath, PdfEncodings.IDENTITY_H);
-        canvas.setFontAndSize(font, 9);
+        canvas.setFontAndSize(font, 8);
 
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
+        /**************************************************************************************************************/
 
         //№
-        canvas.beginText().setTextMatrix(190, 354.33f).showText("nömrə - üöğıəçş ÜÖĞIƏÇŞ").endText();
+        String strNo = "qwertyuiop";
+        setText(strNo, 190f, 238.11f, 358.33f);
+
+
+        /**************************************************************************************************************/
+
+
+        /**
+         **                               Buraxılış vəsiqəsi / Access card
+         **/
 
         //Seriyası və nömrəsi üst
-        canvas.beginText().setTextMatrix(87.87f, 303.30f).showText("seriya üst").endText();
+        String strSeriyaUst = "seriya üst";
+        setText(strSeriyaUst, 87.87f, 226.77f, 305.30f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Seriyası və nömrəsi alt
-        canvas.beginText().setTextMatrix(87.87f, 280.67f).showText("seriya alt").endText();
+        String seriyaAlt = "seriya alt";
+        setText(seriyaAlt, 87.87f, 226.77f, 285.46f);
 
-        //Markası
-        canvas.beginText().setTextMatrix(274.96f, 300.47f).showText("markası").endText();
 
-        //Növü
-        canvas.beginText().setTextMatrix(260.79f, 272.12f).showText("növü").endText();
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - FROM GÜN
-        canvas.beginText().setTextMatrix(90.71f, 252.28f).showText("dd").endText();
+        String ddFrom = "dd";
+        setText(ddFrom, 90.71f, 107.71f, 255.28f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - FROM AY
-        canvas.beginText().setTextMatrix(110.55f, 252.28f).showText("mm").endText();
+        String mmFrom = "mm";
+        setText(mmFrom, 110.55f, 133.22f, 255.28f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - FROM IL
-        canvas.beginText().setTextMatrix(147.40f, 252.28f).showText("yy").endText();
+        String yyFrom = "yy";
+        setText(yyFrom, 147.40f, 161.57f, 255.28f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - TO GUN
-        canvas.beginText().setTextMatrix(90.71f, 232.44f).showText("dd").endText();
+        String ddTo = "dd";
+        setText(ddTo, 90.71f, 107.71f, 235.44f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - TO AY
-        canvas.beginText().setTextMatrix(110.55f, 232.44f).showText("mm").endText();
+        String mmTo = "mm";
+        setText(mmTo, 110.55f, 133.22f, 235.44f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - TO IL
-        canvas.beginText().setTextMatrix(147.40f, 232.44f).showText("yy").endText();
+        String yyTo = "yy";
+        setText(yyTo, 147.40f, 161.57f, 235.44f);
 
-        //DQN - dövlət qeydiyyat nişanı
-        canvas.beginText().setTextMatrix(331.65f, 239.11f).showText("DQN").endText();
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Operator üst
-        canvas.beginText().setTextMatrix(87.87f, 201.26f).showText("operator üst").endText();
+        String operatorUst = "operator üst";
+        setText(operatorUst, 87.87f, 226.77f, 204.26f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Operator alt
-        canvas.beginText().setTextMatrix(87.87f, 181.42f).showText("operator alt").endText();
+        String operatorAlt = "operator alt";
+        setText(operatorAlt, 87.87f, 226.77f, 184.42f);
+
+
+        /**************************************************************************************************************/
+
+
+        /**
+         **                               Avtonəqliyyat vasitəsi / Motor vehicle
+         **/
+
+
+        //Markası
+        String marka = "markasi";
+        setText(marka, 274.96f, 382.68f, 306f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
+
+        //Növü
+        String novu = "novu";
+        setText(novu, 260.79f, 382.68f, 276.12f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
+
+        //DQN - dövlət qeydiyyat nişanı
+        String dqn = "DQN";
+        setText(dqn, 331.65f, 382.67f, 244.11f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Daşımanın növü
-        canvas.beginText().setTextMatrix(235.28f, 181.42f).showText("daşımanın növü").endText();
+        String dasinmaNovu = "daşımanın növü";
+        setText(dasinmaNovu, 235.28f, 382.67f, 184.42f);
 
-        //Qüvvədə olma müddəti - FROM GUN
-        canvas.beginText().setTextMatrix(90.71f, 99.21f).showText("dd").endText();
+
+        /**************************************************************************************************************/
+
+
+        /**
+         **                               Buraxılış kartı / Access card
+         **/
+
+
+        //////////////////////////////////////////////////////////////////////////
+
+
+        //Qüvvədə olma müddəti - FROM GÜN
+        String ddFromAlt = "dd";
+        setText(ddFromAlt, 90.71f, 107.71f, 103.21f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - FROM AY
-        canvas.beginText().setTextMatrix(110.55f, 99.21f).showText("mm").endText();
+        String mmFromAlt = "mm";
+        setText(mmFromAlt, 110.55f, 133.22f, 103.21f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - FROM IL
-        canvas.beginText().setTextMatrix(147.40f, 99.21f).showText("yy").endText();
+        String yyFromAlt = "yy";
+        setText(yyFromAlt, 147.40f, 161.57f, 103.21f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - TO GUN
-        canvas.beginText().setTextMatrix(90.71f, 79.37f).showText("dd").endText();
+        String ddToAlt = "dd";
+        setText(ddToAlt, 90.71f, 107.71f, 83.37f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - TO AY
-        canvas.beginText().setTextMatrix(110.55f, 79.37f).showText("mm").endText();
+        String mmToAlt = "mm";
+        setText(mmToAlt, 110.55f, 133.22f, 83.37f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Qüvvədə olma müddəti - TO IL
-        canvas.beginText().setTextMatrix(147.40f, 79.37f).showText("yy").endText();
+        String yyToAlt = "yy";
+        setText(yyToAlt, 147.40f, 161.57f, 83.37f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Bu sənəd gücləndirilmiş elektron imza ilə... ust
-        canvas.beginText().setTextMatrix(235.28f, 85.04f).showText("bu sənəd üst").endText();
+        String buSenedUst = "bu sənəd üst";
+        setText(buSenedUst, 235.28f, 382.67f, 90.04f);
+
+
+        //////////////////////////////////////////////////////////////////////////
+
 
         //Bu sənəd gücləndirilmiş elektron imza ilə... - alt
-        canvas.beginText().setTextMatrix(235.28f, 68.03f).showText("bu sənəd alt").endText();
+        String buSenedAlt = "bu sənəd alt";
+        setText(buSenedAlt, 235.28f, 382.67f, 73.03f);
 
 
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
-        //////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        /**************************************************************************************************************/
 
 
         Document document = new Document(pdfDocument);
@@ -113,6 +242,17 @@ public class AynaPdf {
         pdfDocument.close();
         pdfWriter.close();
         System.out.println("PDF created");
+    }
+
+    private void setText(String text, float x_axis_start, float x_axis_end, float y_axis) {
+        center = ((x_axis_end - x_axis_start) / 2) + x_axis_start;
+        centerilizedStartPoint = calculateStatPoint(center, text.length());
+        canvas.beginText().setTextMatrix(centerilizedStartPoint, y_axis).showText(text).endText();
+    }
+
+    // for CENTERing contetn
+    private float calculateStatPoint(float x_center_point, int length) {
+        return x_center_point - ((float) length / 2 * CHAR_LENGTH);
     }
 
 }
